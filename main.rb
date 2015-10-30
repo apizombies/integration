d5e73169987a7ce90c519e998f4e20ca4c4a0f5a
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-10-30 13:39:21 +0100

    format

--- main.rb
+++ main.rb
@@ -3,33 +3,33 @@ require 'github/github'
 require 'slack/slack'
 
 delete '/slack/:email' do
-    email = params[:email]
-    slack_token = ENV['slack_token']
-    organization = ENV['organization']
-	slack = Slack.new(slack_token)
-    slack.disable_user(organization, email)
+  email = params[:email]
+  slack_token = ENV['slack_token']
+  organization = ENV['organization']
+  slack = Slack.new(slack_token)
+  slack.disable_user(organization, email)
 end
 
 put '/slack/:email' do
-    email = params[:email]
-    slack_token = ENV['slack_token']
-    organization = ENV['organization']
-	slack = Slack.new(slack_token)
-    slack.invite_user(organization, email)
+  email = params[:email]
+  slack_token = ENV['slack_token']
+  organization = ENV['organization']
+  slack = Slack.new(slack_token)
+  slack.invite_user(organization, email)
 end
 
 delete '/github/:username' do
-    username = params[:username]
-	github_cred = ENV['github_token']
-	organization = ENV['organization']
-    github = Github.new(github_cred)
-    github.dissasociate_user(organization, username)
+  username = params[:username]
+  github_cred = ENV['github_token']
+  organization = ENV['organization']
+  github = Github.new(github_cred)
+  github.dissasociate_user(organization, username)
 end
 
 put '/github/:username' do
-    username = params[:username]
-	github_cred = ENV['github_token']
-	organization = ENV['organization']
-    github = Github.new(github_cred)
-    github.asociate_user(organization, username)
+  username = params[:username]
+  github_cred = ENV['github_token']
+  organization = ENV['organization']
+  github = Github.new(github_cred)
+  github.asociate_user(organization, username)
 end
